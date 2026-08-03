b54680a3f154a98acbb561d71b33f0ff8045f617
Jaeymo <jaey.cyber@gmail.com>
2026-08-03 11:16:45 -0500

    refactor(cleanup): optimize and clean code/types

--- Jaegar/Packages/Packet.lua
+++ Jaegar/Packages/Packet.lua
@@ -1,1 +1,3 @@
-return require(script.Parent._Index["jaeymo_suphi-packet@0.1.0"]["suphi-packet"])
+local REQUIRED_MODULE = require(script.Parent._Index["jaeymo_suphi-packet@0.1.0"]["suphi-packet"])
+export type Packet<A... , B... > = REQUIRED_MODULE.Packet<A... , B... >
+return REQUIRED_MODULE
