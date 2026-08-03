f1e06d64f9ee7c31b4f450cff5d24c1bda235f66
Jaeymo <jaey.cyber@gmail.com>
2026-08-03 11:42:46 -0500

    bumpp 1.7.2

--- Jaegar/Packages/Packet.lua
+++ Jaegar/Packages/Packet.lua
@@ -1,3 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent._Index["jaeymo_suphi-packet@0.1.0"]["suphi-packet"])
-export type Packet<A... , B... > = REQUIRED_MODULE.Packet<A... , B... >
-return REQUIRED_MODULE
+return require(script.Parent._Index["jaeymo_suphi-packet@0.1.0"]["suphi-packet"])
